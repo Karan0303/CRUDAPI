e287ca3005a779b700d1023a73ae61f025097138
Karan Patel <VCSDEV0277@VALUECHAIN.ORG>
2024-06-27 14:07:07 +0530

    crud added

---
+++ Controllers/StudentAPIController.cs
@@ -0,0 +1,69 @@
+﻿using CRUDAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly SqltrainingContext context;
+
+        public StudentAPIController(SqltrainingContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<KStudent>>> GetStudents()
+        {
+            var data = await context.KStudents.ToListAsync();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<KStudent>> GetStudentById(int id)
+        {
+            var student = await context.KStudents.FindAsync(id);
+            if (student==null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<KStudent>> CreateStudent(KStudent std)
+        {
+            await context.KStudents.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<KStudent>> UpdateStudent(int id,KStudent std)
+        {
+            if (id != std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State=EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<KStudent>> DeleteStudent(int id)
+        {
+            var std = await context.KStudents.FindAsync(id);
+            if(std == null)
+            {
+                return NotFound();
+
+            }
+            context.KStudents.Remove(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+
+        }
+    }
+}
---
+++ Models/SqltrainingContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPI.Models;
+
+public partial class SqltrainingContext : DbContext
+{
+    public SqltrainingContext()
+    {
+    }
+
+    public SqltrainingContext(DbContextOptions<SqltrainingContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<KStudent> KStudents { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+       
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<KStudent>(entity =>
+        {
+            entity.ToTable("K_Student");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
